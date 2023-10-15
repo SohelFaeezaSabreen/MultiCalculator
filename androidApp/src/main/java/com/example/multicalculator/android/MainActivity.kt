00e74383169470ec208edf2e7884cf8e089a5a41
SohelFaeezaSabreen <A00288661@mycambrian.ca>
2023-10-14 22:17:13 -0400

    CalcDisplay

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -12,6 +13,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.multicalculator.Greeting
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.text.font.FontWeight
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,8 +56,11 @@ fun CalcRow(){
 }
 
 @Composable
-fun CalcDisplay(){
-
+fun CalcDisplay(display: MutableState<String>){
+    Text(
+        text = display.value, modifier = Modifier.height(50.dp).fillMaxWidth().padding(5.dp),
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Composable
