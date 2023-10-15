c641582e72e3160bf0ae9ea49f75883e6de0fc49
SohelFaeezaSabreen <A00288661@mycambrian.ca>
2023-10-14 22:09:41 -0400

    CalcOperationButton

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -63,8 +63,11 @@ fun CalcNumericButton(){
 }
 
 @Composable
-fun CalcOperationButton(){
-
+fun CalcOperationButton(operation: String, display: MutableState<String>){
+    Button(onClick = { },
+        modifier = Modifier.padding(4.dp)) {
+        Text(text = operation)
+    }
 }
 
 @Composable
